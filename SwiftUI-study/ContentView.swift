b08579d266d4c984f899ebe3503e7ad008e6cb12
Takeshi Fujiki <takecian@gmail.com>
2019-12-02 15:03:20 +0900

    Initial Commit

---
+++ SwiftUI-study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-study
+//
+//  Created by Fujiki Takeshi on 2019/12/02.
+//  Copyright © 2019 Fujiki Takeshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-studyTests/SwiftUI_studyTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_studyTests.swift
+//  SwiftUI-studyTests
+//
+//  Created by Fujiki Takeshi on 2019/12/02.
+//  Copyright © 2019 Fujiki Takeshi. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_study
+
+class SwiftUI_studyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
